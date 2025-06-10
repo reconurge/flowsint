78f950d69937e88efb9a22383b8596c23f6fc596
Eliott Morcillo <eliott.morcillo@gmail.com>
2025-06-10 11:31:16 +0200

    feat: react force graph and realtime logs

--- flowsint-app/src/renderer/src/components/layout/sidebar.tsx
+++ flowsint-app/src/renderer/src/components/layout/sidebar.tsx
@@ -29,7 +29,7 @@ export function Sidebar() {
     const navItems: NavItem[] = [
         { icon: Home, label: "Dashboard", href: "/dashboard" },
         { icon: Fingerprint, label: "Cases", href: "/dashboard/investigations" },
-        { icon: BrickWall, label: "Walls", href: "/dashboard/investigations/wall" },
+        // { icon: BrickWall, label: "Walls", href: "/dashboard/investigations/wall" },
         { icon: Workflow, label: "Transforms", href: "/dashboard/transforms" },
     ]
 
